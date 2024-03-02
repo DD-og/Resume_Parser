80cfa160dc2744891c1d2da2784c8eb16dce2c19
DD-og <150532264+DD-og@users.noreply.github.com>
2024-03-02 12:00:12 +0530

    Add files via upload

---
+++ doc2pdf.py
@@ -0,0 +1,10 @@
+from docx2pdf import convert
+
+# Provide full paths for both input and output files
+input_file_path = r"C:\Resume Parser\Tanuja Namburi.docx"
+output_file_path = r"C:\Resume Parser\resume_folder\output3.pdf"
+
+# Convert DOCX to PDF
+convert(input_file_path, output_file_path)
+
+# Program finished
